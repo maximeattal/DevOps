96775a3561bd29982b4cad32214a2a28f79bb52b
kevinZG-dev <64935494+kevinZG-dev@users.noreply.github.com>
2021-11-10 09:13:14 +0100

    lab6

--- lab6_dockers-containers/assets/hello-world-docker/server.js
+++ lab6_dockers-containers/assets/hello-world-docker/server.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World from docteur !");
+  res.send("Hello World from kevin docker !");
 });
 
 app.listen(PORT);
